2699d43dda60b913ade09ca14f27101667458c06
JB <jb@levillageducode.fr>
2021-10-27 12:39:21 +0200

    Add bottom tab navigator

--- screens/BooksListScreen.js
+++ screens/BooksListScreen.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import SplashScreen from './SplashScreen';
+import BooksScreen from './BooksScreen';
+import SettingsScreen from './SettingsScreen';
+
+
+const Tab = createBottomTabNavigator();
+
 
 export default function BooksListScreen() {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <Tab.Navigator initialRouteName="Books">
+            <Tab.Screen name="Books" component={BooksScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
     )
 }
---
+++ screens/BooksScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function BooksScreen() {
+    return (
+        <View>
+            <Text>Débile</Text>
+        </View>
+    )
+}
